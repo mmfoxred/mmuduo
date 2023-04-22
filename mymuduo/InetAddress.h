352eba2e8957b3dfc2b5dc8a033f72af65de0694
oucat <tangihua@qq.com>
2023-04-22 10:18:54 +0800

    InetAddress测试完成

---
+++ mymuduo/InetAddress.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <netinet/in.h>
+
+#include <string>
+
+class InetAddress {
+public:
+    explicit InetAddress(short port, std::string ipv4 = "127.0.0.1");
+    explicit InetAddress(const sockaddr_in &addr);
+
+    std::string toIp() const;
+    short toPort() const;
+    std::string toIpPort() const;
+
+    const sockaddr_in *getSockAddr() const { return &m_addr; }
+    void setSockAddr(const sockaddr_in &sockaddr) { m_addr = sockaddr; }
+
+private:
+    sockaddr_in m_addr;
+};
