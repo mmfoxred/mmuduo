c53a17b2058225d3d8cde23b3b753f845da25966
mmfoxred <tangihua@qq.com>
2023-05-03 11:23:38 +0800

    自动安装脚本编写

--- mymuduo/install.sh
+++ mymuduo/install.sh
@@ -2,30 +2,32 @@
 set -e
 
 #判断并新建build目录
-if [! -d 'pwd'/build]; then
-    mkdir 'pwd'/build
+if [ ! -d `pwd`/build ]; then #这是Esc下的符号，不是单引号
+    mkdir `pwd`/build
 fi
 
 #清理build
-rm -rf 'pwd'/build/*
+rm -rf `pwd`/build/*
 
 #编译
-cd 'pwd'/build && make ..
+make clean && make
 
 #安装 头文件 so文件
-cd ..
-if [! -d /usr/include/mymuduo]; then
+if [ ! -d /usr/include/mymuduo ]; then
     mkdir /usr/include/mymuduo
+fi
+if [ ! -d /usr/lib/mymuduo ]; then 
+    mkdir /usr/lib/mymuduo
 fi
 
 #头文件
-for header in 'ls *.h'
+for header in `ls *.h`
 do
     cp $header /usr/include/mymuduo
 done
 
 #so库文件
-cp 'pwd'/build/*.so /usr/lib
+cp `pwd`/build/libmymuduo.so /usr/lib/mymuduo
 
 #刷新缓存
 ldconfig
