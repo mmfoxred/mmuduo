d8e7bcfcb7a3702c9302ffb346136679a76ce95d
mmfoxred <tangihua@qq.com>
2023-05-03 14:55:53 +0800

    修改端口ip默认值位置

--- mymuduo/InetAddress.cpp
+++ mymuduo/InetAddress.cpp
@@ -4,18 +4,18 @@
 #include <netinet/in.h>
 #include <strings.h>
 #include <sys/socket.h>
-
 #include <iostream>
 #include <string>
 
-InetAddress::InetAddress(short port, std::string ipv4) {
+InetAddress::InetAddress(std::string ipv4, short port) {
     bzero(&m_addr, sizeof(m_addr));
     m_addr.sin_family = AF_INET;
     m_addr.sin_port = htons(port);
     m_addr.sin_addr.s_addr = inet_addr(ipv4.c_str());
 }
 
-InetAddress::InetAddress(const sockaddr_in &addr) : m_addr(addr) {}
+InetAddress::InetAddress(const sockaddr_in& addr) : m_addr(addr) {
+}
 
 std::string InetAddress::toIp() const {
     char buf[33] = {0};  // ipv4地址 32bit
@@ -23,7 +23,9 @@ std::string InetAddress::toIp() const {
     return buf;
 }
 
-short InetAddress::toPort() const { return ntohs(m_addr.sin_port); }
+short InetAddress::toPort() const {
+    return ntohs(m_addr.sin_port);
+}
 std::string InetAddress::toIpPort() const {
     return toIp() + ":" + std::to_string(toPort());
 }
--- mymuduo/InetAddress.h
+++ mymuduo/InetAddress.h
@@ -5,15 +5,15 @@
 
 class InetAddress {
 public:
-    explicit InetAddress(short port=6000, std::string ipv4 = "127.0.0.1");
-    explicit InetAddress(const sockaddr_in &addr);
+    explicit InetAddress(std::string ipv4 = "127.0.0.1", short port = 6000);
+    explicit InetAddress(const sockaddr_in& addr);
 
     std::string toIp() const;
     short toPort() const;
     std::string toIpPort() const;
 
-    const sockaddr_in *getSockAddr() const { return &m_addr; }
-    void setSockAddr(const sockaddr_in &sockaddr) { m_addr = sockaddr; }
+    const sockaddr_in* getSockAddr() const { return &m_addr; }
+    void setSockAddr(const sockaddr_in& sockaddr) { m_addr = sockaddr; }
 
 private:
     sockaddr_in m_addr;
