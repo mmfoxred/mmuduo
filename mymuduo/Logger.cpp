2ae6c98fa3418af5088ce53a34a02596ffc3dcfd
mmfoxred <tangihua@qq.com>
2023-05-09 21:31:01 +0800

    注释掉main

--- mymuduo/Logger.cpp
+++ mymuduo/Logger.cpp
@@ -29,10 +29,10 @@ void Logger::log(std::string msg) const {
     std::cout << Timestamp::now().toString() << msg;  // 格式化应该留给调用者
 }
 
-int main() {
-    Logger logger;
-    LOG_INFO("%s\n", "hello,test loginfo");
-    LOG_ERROR("%s,%d\n", "hello,test loginfo", LOGGERLEVEL::ERROR);
-    LOG_FATAL("%s,%d\n", "hello,test loginfo", LOGGERLEVEL::FATAL);
-    return 0;
-}
+// int main() {
+//     Logger logger;
+//     LOG_INFO("%s\n", "hello,test loginfo");
+//     LOG_ERROR("%s,%d\n", "hello,test loginfo", LOGGERLEVEL::ERROR);
+//     LOG_FATAL("%s,%d\n", "hello,test loginfo", LOGGERLEVEL::FATAL);
+//     return 0;
+// }
